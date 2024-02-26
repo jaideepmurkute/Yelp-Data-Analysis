2b9ca28f5aa6ba3bc20026d949200a2a6d5822de
Jaideep Murkute <jaideepm.111@gmail.com>
2024-02-26 11:18:15 +0530

    Completed basic EDA

---
+++ CFG.py
@@ -0,0 +1,11 @@
+
+class Config:
+    def __init__(self):
+        self.config_dict = {
+            'data_dir': 'C:\\Users\\Vitthal\\Desktop\\projects\\yelp_data'
+        }
+    
+    def get_config(self):
+        return self.config_dict
+
+
